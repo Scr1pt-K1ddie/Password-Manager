ea567b9147e24ed5f8f7c904f38f407503556329
Scr1pt-K1ddie <94982193+Scr1pt-K1ddie@users.noreply.github.com>
2021-11-26 20:47:46 +0500

    Added the code for the basic interface

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void interface();
+void separators();
 
 int main() {
     int choice;
 
-    printf("_______________________________________________________________");
-    printf("\n\n\t\tWelcome to Password Manager\n\n");
-    printf("_______________________________________________________________");
-    printf("\n\n\t1 for existing user  OR  2 to create new user");
+    interface();
     printf("\n\nEnter your choice:  ");
     scanf("%d", &choice);
 
     switch (choice) {
         case 1:
-            printf("Case1");
+            system("cls");//for clearing the output screen
+            separators();
+            printf("\n\n\tYou can login to your vault here\n\n");
+            separators();
             break;
 
         case 2:
@@ -22,4 +26,15 @@ int main() {
 
 
     return 0;
+}
+
+void interface() {
+    printf("_______________________________________________________________");
+    printf("\n\n\t\tWelcome to Password Manager\n\n");
+    printf("_______________________________________________________________");
+    printf("\n\n\t1 for existing user  OR  2 to create new user");
+}
+
+void separators() {
+    printf("_______________________________________________________________");
 }
