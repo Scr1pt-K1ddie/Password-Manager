7629a7fa757cc2a887e9d5d524e0142b02420c08
Scr1pt-K1ddie <94982193+Scr1pt-K1ddie@users.noreply.github.com>
2021-11-27 22:12:17 +0500

    fixed an issue in main file

--- main.c
+++ main.c
@@ -67,6 +67,8 @@ int signup(char Username[SIZE], char Master_Password[SIZE]) {
     // closing the file
     fclose(fpointer);
 }
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EmptyDeclOrStmt"
 char Identification(char Username[SIZE], char Master_Password[SIZE]) {
     char Verification[SIZE] = "Access Granted";
     char *p;// pointer for storing extracted newline from get_line
@@ -88,13 +90,20 @@ char Identification(char Username[SIZE], char Master_Password[SIZE]) {
 
         // comparing the username with the get_line, if the get_line==Username then access granted
         if ((strcmp(get_line, Username) == 0)) {
-            printf("\n\nAccess Granted.\n\n");
-            return '1';
+            while (fgets(get_line, sizeof(get_line), fpointer)) {
+                if ((p = strchr(get_line, '\n')) != NULL) {
+                    *p = '\0';
+                }
+                if ((strcmp(get_line, Master_Password) == 0)) {
+                    printf("Access granted");
+                    return 0;
+                }
+            }
         }
         // end of the loop body
     }
-    printf("\n\nAccess Denied.\n\nInvalid UserName/password or Vault does not exist\n\n");
     return '0';
     // closing the file
     fclose(fpointer);
 }
+#pragma clang diagnostic pop
